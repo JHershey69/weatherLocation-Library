f778a04fdbea5d31889f6dcf530db779d092c8fe
JHershey69 <60907719+JHershey69@users.noreply.github.com>
2020-02-13 13:48:35 -0500

    Update getLocationonly.ino

--- examples/Get Location Only/getLocationonly.ino
+++ examples/Get Location Only/getLocationonly.ino
@@ -1,12 +1,10 @@
-// The first two includes are required
-// The TFT include is for the ESP32 use whatever you need
-// This is just an example to show you how to get the vars
+// This is an example to show you how to get the vars
 // You need to provide the keys and other defines as indicated for this to work
 
 #include <weatherLocation.h>
 #include <WiFi.h>
-#include <SPI.h>
-#include <TFT_eSPI.h>
+
+
 
 
 weatherLocation WL;
@@ -37,8 +35,8 @@ void setup() {
 
 WL.getCoordinates(GOOGLEKEY); //<-------------------- Gets ONLY LOCATION variables, no weather
 
-// Returns number of wifi networks found before printing these variables
-// In case you are wondering what that rogue number means
+// Serial Monitor will show connection success and the number of local WiFi Signals found
+// Weather Variables return nothing, just zeros
 
 Serial.println("LOCATION VARS");
 Serial.print("WL.latitude: ");
@@ -50,19 +48,6 @@ Serial.println(WL.city);        // char[45]
 Serial.print("WL.state: ");
 Serial.println(WL.state);
 
-Serial.println("WEATHER VARS");
-Serial.print("WL.temperature: ");
-Serial.println(WL.temperature); // int value
-Serial.print("WL.humidity: ");
-Serial.println(WL.humidity); // float value
-Serial.print("WL.uvIndex: ");
-Serial.println(WL.uvIndex); // int value
-Serial.print("WL.windSpeed: ");
-Serial.println(WL.windSpeed);   // float value
-Serial.print("WL.windGust: ");
-Serial.println(WL.windGust); // float value
-Serial.print("WL.windBearing: ");
-Serial.println(WL.windBearing); // int value 0 to 359 degrees
 
 
 }
