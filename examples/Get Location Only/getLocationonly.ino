c35955c09e7d0d01f8d7b1434f5a05543ab1dc16
JHershey69 <60907719+JHershey69@users.noreply.github.com>
2020-02-13 14:32:24 -0500

    v1.04

--- examples/Get Location Only/getLocationonly.ino
+++ examples/Get Location Only/getLocationonly.ino
@@ -1,5 +1,7 @@
 // This is an example to show you how to get the vars
 // You need to provide the keys and other defines as indicated for this to work
+// This example assumes you print to a Serial Monitor if you don't and doing so will
+// break something, please change to print destination as required
 
 #include <weatherLocation.h>
 #include <WiFi.h>
